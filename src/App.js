2fdc9d4ae26e165c7f6e2170d6af0a5d4d5d73ff
dura <dureguye2@gmail.com>
2024-07-31 03:50:46 +0300

    navbar with sidebar mobile menu

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { ThemeProvider } from './components/ThemeContext';
 
-function App() {
+import Navbar from './components/Navbar';
+import Content from './components/Content';
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Navbar />
+      <Content />
     </div>
   );
-}
+};
 
-export default App;
+const AppWrapper = () => (
+  <ThemeProvider>
+    <App />
+  </ThemeProvider>
+);
+
+export default AppWrapper;
---
+++ src/components/Content.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Content = () => {
+  return (
+    <div className="mt-16">
+      
+    </div>
+  );
+};
+
+export default Content;
